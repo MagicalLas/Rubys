30cb8edbb2e024cc9fc9cc6b1025a7ca4a75ad40
Las-Wonho <haho013039@gmail.com>
2018-07-25 11:36:34 +0900

    Ruby Can for Rsing Array, Range

---
+++ Source/28_Range_Array_For_Loop.rb
@@ -0,0 +1,13 @@
+arr = ((((1..6).to_a).reverse)<<0)
+for i in arr
+    print(i,", ")
+end
+puts ""
+for i_ in 3..7
+    print(i_,", ")
+end
+puts ""
+(5..8).each do |i|
+    print(i,", ")
+end
+puts ""
