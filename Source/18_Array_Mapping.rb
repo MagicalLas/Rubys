70ea2041f491880c1ec969ae24f47f595f89030a
Las-Wonho <haho013039@gmail.com>
2018-07-25 10:11:11 +0900

    Add New Map Using Change Origin Array

--- Source/18_Array_Mapping.rb
+++ Source/18_Array_Mapping.rb
@@ -1,8 +1,14 @@
 arr = [1,2,3,4,5,6,7,8,9]
 
 arr_ = arr.map{|item| item+2}
-
+puts "origin Array"
 arr.each{|i|print i , ", "}
 puts ""
+puts "tmp Array"
 arr_.each{|i|print i, ", "}
+puts ""
+
+puts "origin Array"
+arr.map!{|i|i+1}
+arr.each{|i|print i, ", "}
 puts ""
